365f6b47e2ce2441baaf58c8c9859a49ed632a28
Luis Geniole <luis.geniole@gmail.com>
2020-05-04 01:02:35 -0300

    Gnight!

--- prettier.config.js
+++
@@ -1,5 +0,0 @@
-mudule.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  allowParens: 'avoid'
-}
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+
+export default Dashboard;
